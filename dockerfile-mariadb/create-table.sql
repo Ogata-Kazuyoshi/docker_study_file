96c5326384c34241c5e49ae69f21753e533c20d6
Ogata-Kazuyoshi <kazuyoshi_ogata_ab@mail.toyota.co.jp>
2024-01-05 14:54:53 +0900

    mariadbへNodeからアクセス（データ挿入)の練習終了

---
+++ dockerfile-mariadb/create-table.sql
@@ -0,0 +1,12 @@
+-- CREATE TABLE persons (
+--     id int,
+--     lastname varchar(255),
+--     firstname varchar(255),
+--     address varchar(255),
+--     city varchar(255)
+-- );
+CREATE TABLE myTable (
+  id int(11),
+  note varchar(50),
+  PRIMARY KEY (id)
+);
