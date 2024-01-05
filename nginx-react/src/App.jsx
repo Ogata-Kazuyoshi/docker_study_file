e175b0d13d6a6ef1d29c2ebdaa537f891718e74d
Ogata-Kazuyoshi <kazuyoshi_ogata_ab@mail.toyota.co.jp>
2024-01-05 11:42:20 +0900

    Dockerの勉強ファイル

---
+++ nginx-react/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  const checkClick = () => {
+    console.log('clickされたがや');
+  };
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <button onClick={checkClick}>
+        ClickMe!!!!編集も即座に反映されます！！！
+      </button>
+    </>
+  );
+}
+
+export default App;
---
+++ nginx-study/index.js
@@ -0,0 +1,4 @@
+const divTag = document.getElementById('js-hook');
+const pTag = document.createElement('p');
+pTag.innerText = 'JSから表示しているPタグです!!!!!マジで！！！';
+divTag.appendChild(pTag);
